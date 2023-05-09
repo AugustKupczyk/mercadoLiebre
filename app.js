74749bfa5ea6d3087d35d7d3ae461e494ac523e6
AugustKupczyk <augustkup@gmail.com>
2023-05-08 21:49:30 -0300

    añado mi proyecto al repo remoto

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const app = express();
+const path = require ("path");
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.listen(port,()=>{
+    console.log("Servidor escuchando en el puerto" + port);
+});
+
+app.get("/",(req,res) =>{
+    res.send("Anda a Home!");
+})
+
+app.get("/home", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+});
+
+app.get("/register", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/register.html"));
+});
+
+app.get("/login", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/login.html"));
+});
